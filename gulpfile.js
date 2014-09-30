022e694955b15f9e39645d5cbb1ace6bc3d8b6b5
Greg <gregory.pratt@me.com>
2014-09-30 15:34:02 +0100

    Removal of ionic app related stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
 var connect = require('gulp-connect');
 
 var paths = {
@@ -37,24 +34,4 @@ gulp.task('connect', function () {
 
 gulp.task('watch', function () {
 	gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function () {
-	return bower.commands.install()
-		.on('log', function (data) {
-			gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-		});
-});
-
-gulp.task('git-check', function (done) {
-	if (!sh.which('git')) {
-		console.log(
-				'  ' + gutil.colors.red('Git is not installed.'),
-			'\n  Git, the version control system, is required to download Ionic.',
-			'\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-				'\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-		);
-		process.exit(1);
-	}
-	done();
 });
