2f9c3b0e2c54602100111649fb11f7ac61c627bb
Greg <gregory.pratt@me.com>
2014-10-07 16:00:12 +0100

    Added JS Hint

--- www/js/app.js
+++ www/js/app.js
@@ -13,21 +13,21 @@ angular.module('convergence', [
 	.constant('HINT', {
 		small: 0.7,
 		large: 0.65
+	})
+
+.run(function ($ionicPlatform) {
+	'use strict';
+
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
 	});
-
-	//.run(function ($ionicPlatform) {
-	//	$ionicPlatform.ready(function () {
-	//		'use strict';
-
-	//		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-	//		// for form inputs)
-	//		if (window.cordova && window.cordova.plugins.Keyboard) {
-	//			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-	//		}
-	//		if (window.StatusBar) {
-	//			// org.apache.cordova.statusbar required
-	//			StatusBar.styleDefault();
-	//		}
-	//	});
-	//});
+});
 
